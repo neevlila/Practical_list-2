4f7b797c8ebbde4bf7d5bb4aa60c8ca1ba35e3b3
Neev lila <141387755+neevlila@users.noreply.github.com>
2023-09-11 10:50:30 +0530

    Update c1.cpp

--- c1.cpp
+++ c1.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
-int n = 7;// Global variable 
+int n = 7;
 int main()
 {
+
+	cout << "220130318021";
+	
 	cout<<"Enter the value of Global variable is n & its value is  ::  "<<n;
 	return 0;
 }
