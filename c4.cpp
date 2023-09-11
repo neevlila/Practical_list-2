a1e5f8d181ac84396c6422ae892e2b8df202850c
Neev lila <141387755+neevlila@users.noreply.github.com>
2023-09-11 10:53:05 +0530

    Update c4.cpp

--- c4.cpp
+++ c4.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 int main()
 {
-    int n = 10;
 	
-    cout<<"value of a is : "<<n<<endl;
+	cout << "220130318021";
+	
+	int n = 10;
+	
+	cout<<"value of a is : "<<n<<endl;
 	cout<<"This is for function setw : "<<setw(5)<<setfill('*')<<n;
       
 	return 0;
