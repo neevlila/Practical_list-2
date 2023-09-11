fdd895e8c779815f386e142276bb2ef3075999a8
Neev lila <141387755+neevlila@users.noreply.github.com>
2023-09-11 10:55:10 +0530

    Update c8.cpp

--- c8.cpp
+++ c8.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 main()
 {
-	  float km;
 
-	  cout<<"Enter the Kilometer :  ";
-	  cin>>km;
-	  cout<<"Kilometer into Centimeter : "<<km*100000<<endl<<setprecision(5);
-	  cout<<"Kilometer into Meter      : "<<km*1000<<endl<<setprecision(5);
-	  cout<<"Kilometer into Inch       : "<<km*39370.1<<endl<<setprecision(5);;
-	  cout<<"Kilometer into Feet       : "<<km*3280.84<<endl<<setprecision(5);;
+	cout << "220130318021";
+	
+	float km;
+
+	cout<<"Enter the Kilometer :  ";
+	cin>>km;
+	cout<<"Kilometer into Centimeter : "<<km*100000<<endl<<setprecision(5);
+	cout<<"Kilometer into Meter      : "<<km*1000<<endl<<setprecision(5);
+	cout<<"Kilometer into Inch       : "<<km*39370.1<<endl<<setprecision(5);;
+	cout<<"Kilometer into Feet       : "<<km*3280.84<<endl<<setprecision(5);;
 
 	return 0;
 }
