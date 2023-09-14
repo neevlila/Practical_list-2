2026809363ff5bb11d06fa80ccbd1dafd090ba25
Neev lila <141387755+neevlila@users.noreply.github.com>
2023-09-14 08:56:43 +0530

    Add files via upload

---
+++ c14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+inline int cube(int r)
+{
+	return r*r*r;
+}
+int main()
+{
+	int r;
+	
+	cout<<"Enter the value to compute cube: ";
+	cin>>r;
+
+	cout<<"cube of the number: "<<cube(r)<<endl;
+
+	cout<<"Enrollment no.220130318021"<<endl;
+
+	return 0;
+}
