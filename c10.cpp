90ddee3e324e8682f3fa801e371b5e24b659a30d
Neev lila <141387755+neevlila@users.noreply.github.com>
2023-09-14 08:22:37 +0530

    Add files via upload

---
+++ c10.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void func1(int numVal)
+{
+    numVal++;
+}
+void func2(int &numRef) 
+{
+    numRef++;
+}
+
+int main()
+{
+    int num = 5;
+
+    func1(num);
+
+    cout<<"Call by value numval::"<<num<<endl; 
+    
+    func2(num);
+
+	cout<<"Call by Ref numRef::"<<num;
+
+    return 0;
+}
