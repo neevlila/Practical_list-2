4b5dcc4d551a39b23c2c7753655a4287f5a6de91
Neev lila <141387755+neevlila@users.noreply.github.com>
2023-09-14 08:42:33 +0530

    Add files via upload

---
+++ c13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  
+using namespace std;  
+int main ()  
+{   
+	cout<<"Enrollment no.220130318021"<<endl;
+
+	const int x = 17;  
+	const int y = 25;  
+	
+	int z;   
+	z = x + y;  
+	
+	cout << " The sum of the x and y is: " << z << endl;  
+	
+	return 0;  
+}  
