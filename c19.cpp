ca9eb404a67f9387b21fc807f60e4d5f80f38a1f
Neev lila <141387755+neevlila@users.noreply.github.com>
2023-09-14 09:08:43 +0530

    Add files via upload

---
+++ c19.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  
+using namespace std;  
+void change(int data);  
+int main()  
+{  
+cout<<"Enrollment no.220130318021"<<endl\;
+int data = 3;  
+change(data);  
+cout << "Value of the data is: " << data<< endl;  
+return 0;  
+}  
+void change(int data)  
+{  
+data = 5;  
+}
